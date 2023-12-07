96e9ffcf44c6e5cded57dd08e7c1ea5cc366e48f
Trikzmr <109782665+Trikzmr@users.noreply.github.com>
2023-12-07 07:55:19 +0530

    Update hello.c
    just added a scanf function

--- basic/hello.c
+++ basic/hello.c
@@ -4,12 +4,15 @@
 #include <stdlib.h>
 
 int main() {
+   char name;
    char input_string[105]; // Declare a variable named 'input_string' to hold our input.
      
    scanf("%[^\n]", input_string); // Read a full line of input from stdin and save it to our variable, input_string.
      
-   //Your first line of output goes here
-     printf("Hello, World.\n"); 
+   //Your first line of output goes here 
+     printf("Enter your name \n");
+     scanf("%c" &name);
+     printf("hello %c", name);
      printf("%s\n",input_string);
     
     // TODO: Write a line of code here that prints the contents of input_string to stdout.
